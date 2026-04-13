34b57c804279acf00c00b31a28cfd06f7d91ce89
Benjamin Fahl <git@fahl-design.de>
2026-04-13 20:38:39 +0200

    fix(container): ensure has() returns false on circular aliases to satisfy PSR-11

---
+++ tests/Unit/SimpleContainerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebProject\PhpOpenApiMockServer\Tests\Unit;
+
+use Codeception\Test\Unit;
+use WebProject\PhpOpenApiMockServer\Container\Exception\ContainerException;
+use WebProject\PhpOpenApiMockServer\Container\SimpleContainer;
+use WebProject\PhpOpenApiMockServer\Tests\Support\UnitTester;
+
+class SimpleContainerTest extends Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+
+    public function testHasReturnsFalseOnCircularAlias(): void
+    {
+        $container = new SimpleContainer();
+        $container->setAlias('a', 'b');
+        $container->setAlias('b', 'a');
+
+        // Current behavior: this throws ContainerException
+        // Desired behavior: this returns false
+        self::assertFalse($container->has('a'));
+    }
+
+    public function testGetThrowsOnCircularAlias(): void
+    {
+        $container = new SimpleContainer();
+        $container->setAlias('a', 'b');
+        $container->setAlias('b', 'a');
+
+        $this->expectException(ContainerException::class);
+        $this->expectExceptionMessage("Circular alias detected for 'a'.");
+        $container->get('a');
+    }
+}
